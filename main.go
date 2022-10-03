74344774af639d24001f8e7ba069946f52e8a7d4
Higaharu <e205741@ie.u-ryukyu.ac.jp>
2022-10-03 15:36:52 +0900

    second commit

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"gitlab.ie.u-ryukyu.ac.jp/os/2022/e205741-fileWrite/fileWrite/all/OS/os2.1/fileWrite"
+)
+
+func main() {
+	fmt.Println("Hello, World!")
+	fmt.Println(fileWrite.Hello("Higaharu"))
+}
